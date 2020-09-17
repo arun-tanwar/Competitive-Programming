87e1f6fba2647c4ed85d09887b2897d5adf5c9c0
arun-tanwar <46267229+arun-tanwar@users.noreply.github.com>
2020-09-17 16:38:56 -0300

    Create  Finacial_Problem_in_Dholokpur.py

---
+++ Finacial_Problem_in_Dholokpur.py
@@ -0,0 +1,17 @@
+for _ in range(int(input())): 
+   n = int(input()) 
+   price = list(map(int, input().split())) 
+   span = [1] 
+   stack = [0] 
+   for i in range(1, len(price)): 
+       if price[i] < price[stack[-1]]: 
+           span.append(1) 
+       else: 
+           while len(stack) != 0 and price[i] >= price[stack[-1]]: 
+               stack.pop() 
+           if len(stack) == 0: 
+               span.append(i+1) 
+           else: 
+               span.append(i - stack[-1]) 
+       stack.append(i) 
+   print(*span)
